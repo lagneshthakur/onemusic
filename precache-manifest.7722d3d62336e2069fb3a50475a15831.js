501c5e05b2f1fdbfd0e7fb363709fc0081f779dd
vermajai1995 <vermajai1995@gmail.com>
2021-05-22 17:22:34 +0530

    Updates

---
+++ precache-manifest.7722d3d62336e2069fb3a50475a15831.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc9052b0079795d0d08e",
+    "url": "/onemusic/static/css/main.f582108d.chunk.css"
+  },
+  {
+    "revision": "bc9052b0079795d0d08e",
+    "url": "/onemusic/static/js/main.afed43e9.chunk.js"
+  },
+  {
+    "revision": "c44d51ae8bc6fbea2a8b",
+    "url": "/onemusic/static/js/runtime~main.b042a376.js"
+  },
+  {
+    "revision": "b2ddf03093170a9dfc66",
+    "url": "/onemusic/static/js/2.fc6117ce.chunk.js"
+  },
+  {
+    "revision": "5137440fc6abaf34233cad99906100db",
+    "url": "/onemusic/index.html"
+  }
+];
